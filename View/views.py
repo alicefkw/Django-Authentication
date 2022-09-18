6acbcf9fbf61bcc7eeceb825f0d3ea6239e4f85c
Alice Pinna <69218194+alicefkw@users.noreply.github.com>
2022-09-18 15:58:34 -0300

    Add files via upload

---
+++ View/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from users.forms import UserRegistrationForm
+
+def home(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    return render(request, 'logout.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, f'Your account has been created. You can log in now!')    
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'register.html', context)
